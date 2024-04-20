1e03fd9c21d49a4a7f533197d83bbb6683b92f51
Richard Metzler <richard.metzler@endava.com>
2024-04-20 10:00:58 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rmetzler/go-test-import
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/rmetzler/go-test-import/p1"
+	"github.com/rmetzler/go-test-import/p2"
+)
+
+func main() {
+	p1.PrintPackage()
+	p2.PrintPackage()
+}
